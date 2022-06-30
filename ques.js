25ef811ac5c53c1a1023c5f471985165f7b2c5c2
Kush-Sharad <sharadkushwah10@gmail.com>
2022-06-30 22:55:37 +0530

    wrong answer on testcase-2

---
+++ ques.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "To see what heppened, he started to walk ____ the place where the sound had come.",
+    answer: "towards",
+    options: [
+      "onto",
+      "towards",
+      "in",
+      "after"
+    ]
+  },
+    {
+    numb: 2,
+    question: "I have no fear, I can say all the things that I said yesterday ____ his face.",
+    answer: "at",
+    options: [
+      "with",
+      "to",
+      "at",
+      "onto"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Of course you can't see it. The village is ____ the hills.",
+    answer: "behind",
+    options: [
+      "in front of",
+      "below",
+      "behind",
+      "beside"
+    ]
+  },
+    {
+    numb: 4,
+    question: "There are a lot of books in my library which you may need for your course. You can take ___ .",
+    answer: "whichever you like",
+    options: [
+      "however you may like",
+      "whichever you like",
+      "they may be very useful",
+      "rather than reading them"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The high cost ___ living is really getting me down, growled one fellow.",
+    answer: "of",
+    options: [
+      "for",
+      "of",
+      "over",
+      "by"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Your room is a mess, but you ___ clean it if you want to go out with your friends provided that you do it when you come back.",
+    answer: "do not have to",
+    options: [
+      "do not have to",
+      "must",
+      "have to",
+      "must not"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Because of the general pardon most of the prisoners ___ by the end of this year.",
+    answer: "will have been released",
+    options: [
+      "will have been released",
+      "are released",
+      "have been released",
+      "are being released"
+    ]
+  },
+  {
+    numb: 8,
+    question: "The Prime Minister ____ about every particular detail per every five minutes during the operation by the team.",
+    answer: "is to be informed",
+    options: [
+      "informed",
+      "have informed",
+      "having been informed",
+      "is to be informed"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The genius student ___ unhappy sice he found the explanations to his problems unsatisfactory.",
+    answer: "seemed to be",
+    options: [
+      "seems to have been",
+      "has been seemed",
+      "seemed to be",
+      "is seemed to be"
+    ]
+  },
+  {
+    numb: 10,
+    question: "By this time tomarraow, certain results of the elections ___ by the government officials.",
+    answer: "will have been declared",
+    options: [
+      "are to have declared",
+      "will have been declared",
+      "is having been declared",
+      "have been declared"
+    ]
+  },
+  
+];
